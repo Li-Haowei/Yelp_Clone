238061e0576e42828a046edd700a0cd3b06c9e65
Li-Haowei <haoweilee666@gmail.com>
2022-03-28 04:26:06 -0400

    restaurant adapter seems not working, now needs to find new methods for dynamically creating views for user

--- app/src/main/java/com/example/yelpclone/MainActivity.java
+++ app/src/main/java/com/example/yelpclone/MainActivity.java
@@ -1,16 +1,8 @@
 package com.example.yelpclone;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.util.Log;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
