1f88194eede7a99e1a9d1b327de61ed08ebaec74
Li-Haowei <haoweilee666@gmail.com>
2022-03-28 01:20:49 -0400

    debugged url

--- app/src/main/java/com/example/yelpclone/MainActivity.java
+++ app/src/main/java/com/example/yelpclone/MainActivity.java
@@ -3,22 +3,28 @@ package com.example.yelpclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String BASE_URL = "https://api.yelp.com/v3";
+    public static final String BASE_URL = "https://api.yelp.com/v3/";
     public Gson gson;
     public Retrofit retrofit;
+    private YelpService yp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
                 .create();
@@ -26,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
-        retrofit.create(YelpService.class);
+        yp = retrofit.create(YelpService.class);
+        yp.searchRestaurants("Avocado Toast","New York").enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                Log.d("creation", "response.toString()");
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Log.d("creation", "failed");
+            }
+        });
+
     }
 }
