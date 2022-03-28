d27fdef48dd162952f62d211eae6a5787a118891
Li-Haowei <haoweilee666@gmail.com>
2022-03-27 23:14:55 -0400

    Creating the Retrofit instance

--- app/src/main/java/com/example/yelpclone/MainActivity.java
+++ app/src/main/java/com/example/yelpclone/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String BASE_URL = "https://api.yelp.com/v3";
+    public Gson gson;
+    public Retrofit retrofit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .create();
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
     }
 }
